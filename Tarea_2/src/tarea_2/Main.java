a2997853c654895e488f23a396a62ab56e8ad5eb
Bryan Recinos <87162291+brecinosm@users.noreply.github.com>
2023-08-23 20:21:32 -0600

    Add files via upload

---
+++ Tarea_2/src/tarea_2/Main.java
@@ -0,0 +1,9 @@
+package tarea_2;
+// @author brecinosm
+public class Main {
+    public static void main(String[] args) {  
+        
+        frm_principal frm = new frm_principal();
+        frm.setVisible(true);
+    }
+}
